87fc4a7cd43b15b76640be30c7d305f763bbe27a
AnnaLurka <annaklurka@gmail.com>
2020-02-09 10:41:26 +0100

    21.4

---
+++ showtasks.bat
@@ -0,0 +1,19 @@
+call C:\Users\anaka\Downloads\tasks\runcrud.bat
+if "%ERRORLEVEL%" == "0" goto runbrowser
+echo.
+echo runcrud has errors. - breaking work
+goto fail
+
+:runbrowser
+start chrome
+start chrome http://localhost:8080/crud/v1/task/getTasks
+goto end
+
+
+:fail
+echo.
+echo There were errors.
+
+:end
+echo.
+echo Work is finished.
